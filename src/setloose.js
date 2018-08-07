07801e2928417ac45944f6fcfda2dd7b3fe5556e
Jon Apgar <5638966+jonapgar@users.noreply.github.com>
2018-08-07 09:51:26 -0400

    fixes

--- src/setloose.js
+++ src/setloose.js
@@ -1,115 +1,150 @@
-module.exports = function(options){
-	options = options || {};
-	var resolution = options.resolution || 10;
-	var cleanInterval = options.cleanInterval || 5000;
+
+module.exports = options => {
+	options = options || {}
+	const resolution = options.resolution || 10
+	const cleanInterval = options.cleanInterval || 5000
+	const splitResolution = options.splitResolution || 500
+	const minSplits = options.minSplits || 4
+
 	
-	var intervals=[],masters={},hasCleared=0;
-	function tick(){
-		var intervals = this;
-		for (var i=0;i<intervals.length;i++){
-			var v = intervals[i]
+	let masters={}
+	let hasCleared=0
+	let hasAdded=0
+	const tick = intervals=>{
+		let now = Date.now()
+		for (let i=0;i<intervals.length;i++){
+			const v = intervals[i]
 			if (!v) {
 				//do nothing
-			} else if (v.skip){
-				v.skip=false
 			} else {
-				
-				v.f.apply(v.context,v.args)		
+				let start = v.start
+				if (start > now) {
+					continue
+				}
+				delete v.start
+				if (v.cleared) {
+					
+					throw new Error('a cleared timeout should not be here')
+				}
 				if (v.limit){
-					if (v.count>v.limit)
+					if (v.count>=v.limit){
 						clearLooseInterval(v)	
+						continue
+					}
 					else
 						v.count++
-				} 
+				}
+				v.f(v.options)		
 			}
 		}
 		
 	}
 
-	function snap(n){
-		return resolution * Math.round(n/resolution);
-	}
+	const snap = n=>resolution * Math.round(n/resolution)
 
-	function setLooseInterval(f,t,limit,context,args){
-		t = snap(t);
-		var now = Date.now();
+
+	const  setLooseInterval = (f,t,limit,options)=>{
+		t = snap(t)
+		const now = Date.now()
 		if (typeof f !=='function') {
 			
 			throw new Error('must be func')
 		}
-
-		var master = masters[t]
+		const splits =Math.max(minSplits,Math.ceil(t/splitResolution))
+		const splitTime = Math.round(t/splits)
+		const key = `${splitTime}`
+		let master = masters[key]
 		if (!master) {
-			master = masters[t] = {intervals:[],t:t,count:1};
-			master.intervalHandle = setInterval(tick.bind(master.intervals),t);
+			master = masters[key] = {intervals:[],t,count:1,key,f:tick}
+			master.intervalHandle = setInterval(()=>master.f(master.intervals),splitTime)
 		} else {
-			master.count++;
+			master.count++
 		}
-		var v;
-		master.intervals.push(v = {index:intervals.length, masterIndex: master.intervals.length, master: master, f: f, context: context || {}, args: args || [], limit: limit || 0, count: limit ? 0 : undefined, skip: (now - master.last) < t * 0.125 })
-		intervals.push(v);
 		
-		return v;
+		
+		let v
+		master.intervals.push(v = {start:now+t,index: master.intervals.length, master, f, options, limit: limit || 0, count: limit ? 0 : undefined})
+		
+		  
+		hasAdded++
+
+		return v
 	}
 
-	function setLooseTimeout(f,t,context,args){
-		return setLooseInterval(f,t,1,context,args)
+	const setLooseTimeout  = (f,t,options)=>{
+		return setLooseInterval(f,t,1,options)
 	}
 
-	function clearLooseInterval(v){
+	const clearLooseInterval = v=>{
 		if (!v || v.cleared)
-			return;
-		var i = v.index;
-		intervals[i]=undefined;
-		v.master.intervals[v.masterIndex]=undefined;
-		v.master.count--;
+			return
+		
+		v.master.intervals[v.index]=undefined
+		v.master.count--
 		if (v.master.count<1) {
-			masters[v.master.t]=undefined;
+			masters[v.master.key]=undefined
+			v.master.f = ()=>{throw new Error('this master should not be called again')}
 			clearInterval(v.master.intervalHandle)
 		}
-		hasCleared++;
-		v.cleared=true;
+		hasCleared++
+		v.cleared=true
 	}
 
-	setInterval(function(){
-
-		if (hasCleared==0 || hasCleared/intervals.length < 0.5) {
-			
-			return;
-		}
-		//clean up empty entries in intervals
-		//we do this every 
-		var intervals2 = [],masters2={}
+	setInterval(() => {
 		
-		for (var i in masters){
-			var v = masters[i];
-			if (v) {
-				masters2[i]=v;
-				var masterIntervals=v.intervals, masterIntervals2=[];
-				for (var j=0;j<masterIntervals.length;j++){
-					var v2 = masterIntervals[j];
-					if (v2){
-						v2.masterIndex = masterIntervals2.length;
-						v2.index = intervals2.length;
-						masterIntervals2.push(v2)
-						intervals2.push(v2)
+		if (hasCleared==0 || hasCleared/hasAdded < 0.5) {
+			
+			return
+		}
+
+		//clean up empty entries in intervals
+		
+
+		const masters2={}
+		let a = 0
+		let b = 0
+		for (const key in masters){
+			let master = masters[key]
+			if (master) {
+				masters2[key] = master
+				let intervals=[]
+				for (const interval of master.intervals){
+					if (interval){
+						interval.index = intervals.length
+						intervals.push(interval)
 					}
 				}
+				master.intervals = intervals
 			}
 		}
-	
-		
-		
-		intervals=intervals2;
-		masters = masters2;
-		
-		hasCleared=0;
+
+		masters = masters2
+
+		hasAdded-=hasCleared
+		hasCleared=0
+
 	},cleanInterval)
 
 	return {
-		setLooseTimeout:setLooseTimeout,
-		setLooseInterval:setLooseInterval,
-		clearLooseInterval:clearLooseInterval,
+		setLooseTimeout,
+		setLooseInterval,
+		clearLooseInterval,
 		clearLooseTimeout:clearLooseInterval,
 	}
+}
+
+if (!module.parent) {
+	let setLooseTimeout = module.exports({
+		resolution:100,
+		splitResolution:500,
+		minSplits:10,
+	}).setLooseTimeout
+	setInterval(function(){
+		let now = Date.now()
+		setLooseTimeout(opts=>{
+			let delta = Date.now() -now
+			console.log('delta: %dms\t\t%o',delta,opts)
+		},1000)	
+	},100)
+	
 }
